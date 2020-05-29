5f4892e2f8e9e75a3dbbb5b3e3713f7061c6a2c9
pesockiy <loremipsum.pt@gmail.com>
2020-05-29 13:32:57 +0300

    renaming fuctions

--- script.js
+++ script.js
@@ -1,126 +1,132 @@
 const arrayOfAlphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'.split(''),
-      objectToCompare = {},
-			
-      inputName = document.querySelector('#name'),
-      outputName = document.querySelector('#labelForName'),
-			inputDate = document.querySelector('#date'),
-      outputDate = document.querySelector('#labelForDate'),
-			setName = document.querySelectorAll('.num-form__btn')[0],
-			setSoul = document.querySelectorAll('.num-form__btn')[1],
-			textName = document.querySelector('.num-form__label--left'),
-			textSoul = document.querySelector('.num-form__label--right'),
-			
-			reducer = (sum,i) => sum + i;
+	objectToCompare = {},
 
-let flag = true,
-		oldItem = document.querySelector('.active'),
-		timer;
+	inputName = document.querySelector('#name'),
+	outputName = document.querySelector('#labelForName'),
+	inputDate = document.querySelector('#date'),
+	outputDate = document.querySelector('#labelForDate'),
+	setName = document.querySelectorAll('.num-form__btn')[0],
+	setSoul = document.querySelectorAll('.num-form__btn')[1],
+	textName = document.querySelector('.num-form__label--left'),
+	textSoul = document.querySelector('.num-form__label--right'),
 
-const writingText = (item) => {
-  return function() {
+	reducer = (sum, i) => sum + i;
 
-    let  itemVal = item.getAttribute('data-name'),
-        counter = 0;
+let oldItem = document.querySelector('.active'),
+	timer;
 
-    if(item.classList.contains('active')) return;
-    
-    if(oldItem.classList.contains('active')) {
+
+//writing text on labels
+const writingText = item => {
+	return function () {
+
+		let itemVal = item.getAttribute('data-name'),
+			counter = 0;
+
+		if (item.classList.contains('active')) return;
+
+		if (oldItem.classList.contains('active')) {
 			oldItem.classList.remove('active');
 		};
-    
-    item.classList.add('active');
-    item.textContent = '';
-    if (timer) {
-      clearTimeout(timer);
-      oldItem.textContent = oldItem.getAttribute('data-name');
-    }
-    
-      (function() {
 
-        if(counter < itemVal.length) {
-        
-            item.textContent += itemVal[counter];
-            counter++;
-            timer = setTimeout(arguments.callee, 50);
-        
-        };
-      })();
-      oldItem = item;
-  };
+		item.classList.add('active');
+		item.textContent = '';
+
+		if (timer) {
+			clearTimeout(timer);
+			oldItem.textContent = oldItem.getAttribute('data-name');
+		}
+
+		(function () {
+
+			if (counter < itemVal.length) {
+
+				item.textContent += itemVal[counter];
+				counter++;
+				timer = setTimeout(arguments.callee, 50);
+
+			};
+		})();
+
+		oldItem = item;
+
+	};
 };
 
-	textSoul.addEventListener('click', writingText(textSoul));
-	textName.addEventListener('click', writingText(textName));
+textSoul.addEventListener('click', writingText(textSoul));
+textName.addEventListener('click', writingText(textName));
 
 
 let x = 1;
-arrayOfAlphabet.forEach((current) => {
-    objectToCompare[current] = x;
-    if(x === 9) x = 0;
-    x++;
+
+arrayOfAlphabet.forEach(current => {
+
+	objectToCompare[current] = x;
+	if (x === 9) x = 0;
+	x++;
+
 });
 
 const isEng = text => {
-    return /[а-я]/i.test(text);
+
+	return /[а-я]/i.test(text);
+
 };
 
-const countingNum = (element,writingField) => {
-	
-	 if(element.length > 0) {
-        
-        const sumUp = () => {
-					
-            element = element
-							.reduce(reducer)
-							.toString()
-							.split('');
-					
-            element = element.map(item => +item);
-            if(element.length > 1) return sumUp();
-        };
-        
-        console.log(element);
-		 
-        sumUp();
-    };
-	
+const countingNum = (element, writingField) => {
+
+	if (element.length > 0) {
+
+		const sumUp = () => {
+
+			element = element
+				.reduce(reducer)
+				.toString()
+				.split('');
+
+			element = element.map(item => +item);
+			if (element.length > 1) return sumUp();
+		};
+
+		sumUp();
+	};
+
 	writingField.innerText = element;
-	
+
 };
 
 
 //
 const numOfName = () => {
 
-    let newArrFromInput = inputName
-            .value
-            .toLowerCase()
-            .replace(/\s+/g, '')
-            .split(''),
-        sum = '';
-	
-	if(!newArrFromInput.length > 0) return;
+	let newArrFromInput = inputName
+		.value
+		.toLowerCase()
+		.replace(/\s+/g, '')
+		.split(''),
+		sum = '';
 
-	if(!isEng(newArrFromInput)) {
+	if (!newArrFromInput.length > 0) return;
+
+	if (!isEng(newArrFromInput)) {
 		outputName.innerText = 'Введите на русском!';
 	};
-	
-  newArrFromInput.forEach((current) => {
-     sum += objectToCompare[current];
-  });
 
-    let sumUpVal = sum.split('');
-	
-		countingNum(sumUpVal,outputName);
-    
+	newArrFromInput.forEach(current => {
+		sum += objectToCompare[current];
+	});
+
+	let sumUpVal = sum.split('');
+
+	countingNum(sumUpVal, outputName);
+
 };
 
 const numOfSoul = () => {
-	if(!inputDate.value.length > 0) return;
-	countingNum(inputDate.value.split("-"),outputDate);
-	
-}; 
+	if (!inputDate.value.length > 0) return;
+	countingNum(inputDate.value.split("-"), outputDate);
 
-setName.addEventListener('click',numOfName);
-setSoul.addEventListener('click',numOfSoul);
+};
 
+setName.addEventListener('click', numOfName);
+setSoul.addEventListener('click', numOfSoul);
